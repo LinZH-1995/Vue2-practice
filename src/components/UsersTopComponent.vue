dd0004dc5f7aba35f62c466ab0b512a7492f3d0d
LinZH-1995 <lin84610@gmail.com>
2024-02-02 10:48:28 +0800

    add UsersTopView and its components

---
+++ src/components/UsersTopComponent.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    initUser: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data: function () {
+    return {
+      user: this.initUser
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="col-3">
+    <a href="#">
+      <img :src="user.image ?? 'http://via.placeholder.com/300x300?text=No+Image'" width="140px" height="140px">
+    </a>
+    <h2>{{ user.name }}</h2>
+    <span class="badge rounded-pill text-bg-secondary">追蹤人數：{{  user.FollowerCount }}</span>
+    <p class="mt-3">
+      <button type="button" class="btn btn-danger mx-2" v-if="user.isFollowed">
+        取消追蹤
+      </button>
+      <button type="button" class="btn btn-primary" v-else>
+        追蹤
+      </button>
+    </p>
+  </div>
+</template>
--- src/views/UsersTopView.vue
+++ src/views/UsersTopView.vue
@@ -1,9 +1,71 @@
 <script>
 import NavTabsComponent from '../components/NavTabsComponent.vue'
+import UsersTopComponent from '../components/UsersTopComponent.vue'
+
+const dummyData = {
+  "users": [
+    {
+      "id": 1,
+      "name": "root",
+      "email": "root@example.com",
+      "password": "$2a$10$imeDGioZkuOzi7fV/g3I.ekVZS.SecTN2yp93Gx5q0qGuSYd3GIu2",
+      "isAdmin": true,
+      "image": null,
+      "createdAt": "2024-01-31T07:20:03.000Z",
+      "updatedAt": "2024-01-31T07:20:03.000Z",
+      "Followers": [],
+      "FollowerCount": 0,
+      "isFollowed": false
+    },
+    {
+      "id": 2,
+      "name": "user1",
+      "email": "user1@example.com",
+      "password": "$2a$10$RRHRcZKVXOkLHyavs3OZ3uGOMKh3F0TmjiOZ82keF9LlgI/CDcHsy",
+      "isAdmin": false,
+      "image": null,
+      "createdAt": "2024-01-31T07:20:03.000Z",
+      "updatedAt": "2024-01-31T07:20:03.000Z",
+      "Followers": [],
+      "FollowerCount": 0,
+      "isFollowed": false
+    },
+    {
+      "id": 3,
+      "name": "user2",
+      "email": "user2@example.com",
+      "password": "$2a$10$jFzslGypP3tvjKg0iYfVYe/MHKTb.JjUqmVS3jXFZRC.LOm9nuFBW",
+      "isAdmin": false,
+      "image": null,
+      "createdAt": "2024-01-31T07:20:03.000Z",
+      "updatedAt": "2024-01-31T07:20:03.000Z",
+      "Followers": [],
+      "FollowerCount": 0,
+      "isFollowed": false
+    }
+  ]
+}
 
 export default {
   components: {
-    NavTabsComponent
+    NavTabsComponent,
+    UsersTopComponent
+  },
+
+  data: function() {
+    return {
+      users: []
+    }
+  },
+
+  methods: {
+    fetchUsers() {
+      this.users = dummyData.users
+    }
+  },
+
+  created() {
+    this.fetchUsers()
   }
 }
 </script>
@@ -14,5 +76,9 @@ export default {
     <h1 class="mt-5">
       美食達人
     </h1>
+    <hr>
+    <div class="row text-center">
+      <UsersTopComponent v-for="user in users" :key="user.id" :init-user="user"/>
+    </div>
   </div>
 </template>
