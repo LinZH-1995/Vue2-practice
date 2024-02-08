ac1f31c9b8a379f175d59a2424bc99016c21c51b
LinZH-1995 <lin84610@gmail.com>
2024-02-08 13:09:49 +0800

    disable currentUser's follow button

--- src/components/UsersTopComponent.vue
+++ src/components/UsersTopComponent.vue
@@ -1,8 +1,14 @@
 <script>
 import { usersApi } from '../apis/users'
 import { Toast } from '../utils/sweetalert'
+import { useUserStore } from '../stores/user'
 
 export default {
+  setup() {
+    const userStore = useUserStore()
+    return { userStore }
+  },
+
   props: {
     initUser: {
       type: Object,
@@ -44,20 +50,22 @@ export default {
 </script>
 
 <template>
-  <div class="col-3">
+  <div class="col-3 mb-3">
     <router-link :to="{ name: 'user', params: { id: user.id } }">
       <img :src="user.image ?? 'http://via.placeholder.com/300x300?text=No+Image'" width="140px" height="140px">
     </router-link>
     <h2>{{ user.name }}</h2>
     <span class="badge rounded-pill text-bg-secondary">追蹤人數：{{ user.FollowerCount }}</span>
     <p class="mt-3">
-      <button type="button" class="btn btn-danger mx-2" v-if="user.isFollowed"
-        @click.stop.prevent="toggleFollow(user.id)">
-        取消追蹤
-      </button>
-      <button type="button" class="btn btn-primary" v-else @click.stop.prevent="toggleFollow(user.id)">
-        追蹤
-      </button>
+      <template v-if="userStore.currentUser.id !== user.id">
+        <button type="button" class="btn btn-danger mx-2" v-if="user.isFollowed"
+          @click.stop.prevent="toggleFollow(user.id)">
+          取消追蹤
+        </button>
+        <button type="button" class="btn btn-primary" v-else @click.stop.prevent="toggleFollow(user.id)">
+          追蹤
+        </button>
+      </template>
     </p>
   </div>
 </template>
